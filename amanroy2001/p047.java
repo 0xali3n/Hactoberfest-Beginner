29f51b0219902a3a3b36bae68be66213c719c221
AmanRoy <72335987+amanroy2001@users.noreply.github.com>
2021-10-03 23:23:58 +0530

    Create p047.java

---
+++ amanroy2001/p047.java
@@ -0,0 +1,40 @@
+
+public final class p047 implements EulerSolution {
+	
+	public static void main(String[] args) {
+		System.out.println(new p047().run());
+	}
+	
+	
+	public String run() {
+		for (int i = 2; ; i++) {
+			if (       has4PrimeFactors(i + 0)
+			        && has4PrimeFactors(i + 1)
+			        && has4PrimeFactors(i + 2)
+			        && has4PrimeFactors(i + 3))
+				return Integer.toString(i);
+		}
+	}
+	
+	
+	private static boolean has4PrimeFactors(int n) {
+		return countDistinctPrimeFactors(n) == 4;
+	}
+	
+	
+	private static int countDistinctPrimeFactors(int n) {
+		int count = 0;
+		for (int i = 2, end = Library.sqrt(n); i <= end; i++) {
+			if (n % i == 0) {
+				do n /= i;
+				while (n % i == 0);
+				count++;
+				end = Library.sqrt(n);
+			}
+		}
+		if (n > 1)
+			count++;
+		return count;
+	}
+	
+}
