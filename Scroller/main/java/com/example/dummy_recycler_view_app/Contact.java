6f865c6478d1c890f812bdb758bf5881eb5496a4
mearchit <74748787+mearchit@users.noreply.github.com>
2021-10-12 17:29:14 +0530

    Dummy Scroller App using Java

---
+++ Scroller/main/java/com/example/dummy_recycler_view_app/Contact.java
@@ -0,0 +1,35 @@
+package com.example.dummy_recycler_view_app;
+
+import java.util.ArrayList;
+
+public class Contact {
+    final private String mName;
+    final private boolean mOnline;
+
+    public Contact(String name, boolean online) {  //constructor
+
+        mName = name;
+        mOnline = online;
+    }
+
+    public String getName() {
+        return mName;
+    }
+
+    public boolean isOnline() {
+        return mOnline;
+    }
+
+    private static int lastContactId = 0;
+
+    public static ArrayList<Contact> createContactsList(int numContacts) {
+        ArrayList<Contact> contacts = new ArrayList<Contact>();
+
+        for (int i = 1; i <= numContacts; i++) {
+            contacts.add(new Contact("Person " + ++lastContactId, i <= numContacts / 2));
+        }
+
+        return contacts;
+    }
+}
+
