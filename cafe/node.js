ae016208eb5f80b318bc9bbd0cfb7b156900ad24
jay-deep <44156426+jay-deep@users.noreply.github.com>
2021-10-22 14:52:49 +0530

    Added one new Site and one code

---
+++ cafe/node.js
@@ -0,0 +1,15 @@
+// console.log("Komal chutiya hai");
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
