13c57b25f31a704c3ee7d048c866c7f15e67ae33
AbhishekRana <87165058+imabhishekrana@users.noreply.github.com>
2021-10-28 16:25:31 +0530

    Binary Search in an Array

---
+++ Java Programs/binarysearch.java
@@ -0,0 +1,37 @@
+package dsa;
+
+public class binarysearch {
+    public static void main(String[] args) {
+
+        int[] arr = {-18, -12, -4, 0, 2, 3, 4, 15, 16, 18, 22, 45, 89};
+        int target = 22;
+        int ans = binarySearch(arr, target);
+        System.out.println(ans);
+
+
+    }
+
+
+    static int binarySearch(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (target < arr[mid]) {
+                end = mid - 1;
+            } else if (target > arr[mid]) {
+                start = mid + 1;
+            } else {
+                // ans found
+                return mid;
+            }
+
+
+        }
+        return -1;
+    }
+}
+
+
