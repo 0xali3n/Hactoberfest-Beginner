7a9be2c487f31af4db757296fa26389178facafc
s-cs-s <86903321+s-cs-s@users.noreply.github.com>
2021-10-31 18:29:54 +0530

    Update app.js

--- Self_reloading_app/app.js
+++ Self_reloading_app/app.js
@@ -25,33 +25,7 @@ app.get("/",function (req,res) {
     //console.log(today);
 
 });
-// app.get("/w",(req,res)=>{
-//     res.set("Content-Type", "text/html");
-//       //OR
-//     //res.setHeader("Content-Type", "text/html");
-//     res.write("<h2> Thanks for introducing me to the songs of </h2>");
-//     res.write("<h1>AP Dhillon</h1>");
-//     res.write("<p>I'm addicted !</p>");
-//     res.send();
-// })
-app.get("/file",(req,res)=>{
-    res.sendFile(__dirname+"/index.html")///Users/sahilsoni/web/todolist-v1
-    console.log(__dirname)
-})
-/*
-app.get('/user/:uid/photos/:file', function(req, res){
-    var uid = req.params.uid
-      , file = req.params.file;
-  
-    req.user.mayViewFilesFrom(uid, function(yes){
-      if (yes) {
-        res.sendFile('/uploads/' + uid + '/' + file);
-      } else {
-        res.send(403, 'Sorry! you cant see that.');
-      }
-    });
-});
-*/
+
 
 app.listen(3000,function (req,res) {
    console.log("server started on port:3000") 
