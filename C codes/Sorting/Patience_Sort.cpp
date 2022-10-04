31b6c151579be51401bff7407a7c185c54daeb30
Koustubh Mane <114218468+Koustubh-Mane1@users.noreply.github.com>
2022-10-04 23:41:35 +0530

    Added Patience Sort in Sorting Algorithms

--- C codes/Sorting/Patience_Sort.cpp
+++ C codes/Sorting/Patience_Sort.cpp
@@ -4,31 +4,19 @@ using namespace std;
 
 vector<int> merge_piles(vector<vector<int> >& v)
 {
-
 	vector<int> ans;
-
 	while (1) {
-
 		int minu = INT_MAX;
-
 		int index = -1;
-    
 		for (int i = 0; i < v.size(); i++) {
 			if (minu > v[i][v[i].size() - 1]) {
-
 				minu = v[i][v[i].size() - 1];
-
 				index = i;
 			}
 		}
-
-	
 		ans.push_back(minu);
-
-
 		v[index].pop_back();
 		if (v[index].empty()) {
-
 			v.erase(v.begin() + index);
 		}
 		if (v.size() == 0)
@@ -47,7 +35,6 @@ vector<int> patienceSorting(vector<int> arr)
 		}
 		else {
 			int flag = 1;
-
 			for (int j = 0; j < piles.size(); j++) {
 				if (arr[i] < piles[j][piles[j].size() - 1]) {
 					piles[j].push_back(arr[i]);
@@ -58,21 +45,16 @@ vector<int> patienceSorting(vector<int> arr)
       
 			if (flag) {
 				vector<int> temp;
-
 				temp.push_back(arr[i]);
-
 				piles.push_back(temp);
 			}
 		}
 	}
 
 	vector<int> ans;
-
 	ans = merge_piles(piles);
-
 	for (int i = 0; i < ans.size(); i++)
 		cout << ans[i] << " ";
-
 	return ans;
 }
 
