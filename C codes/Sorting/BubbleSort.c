94dd77c35ab922f61d44c837d5c86c4646355372
ad-nat-delt <97732983+ad-nat-delt@users.noreply.github.com>
2022-10-17 21:57:58 +0530

    Update BubbleSort.c
    1.Added ascending in bubble sort description
    2.Added indentation for neatness

--- C codes/Sorting/BubbleSort.c
+++ C codes/Sorting/BubbleSort.c
@@ -8,16 +8,15 @@ void swap(int *xp, int *yp)
 	*yp = temp;
 }
 
-// A function to implement bubble sort
+// A function to implement bubble sort in ascending order
 void bubbleSort(int arr[], int n)
 {
-int i, j;
-for (i = 0; i < n-1; i++)	
-
-	// Last i elements are already in place
-	for (j = 0; j < n-i-1; j++)
-		if (arr[j] > arr[j+1])
-			swap(&arr[j], &arr[j+1]);
+	int i, j;
+	for (i = 0; i < n-1; i++)
+		// Last i elements are already in place
+		for (j = 0; j < n-i-1; j++)
+			if (arr[j] > arr[j+1])
+				swap(&arr[j], &arr[j+1]);
 }
 
 /* Function to print an array */
